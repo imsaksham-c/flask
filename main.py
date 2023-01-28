5d6bbfdf25957b4a5e889ee4fd4fdef705128bcd
imsaksham-c <iimatesak@gmail.com>
2023-01-28 18:53:19 +0530

    Change

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    
+    
+@app.route('/receive_msg', methods=['POST','GET'])
+def webhook():
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token")== "Saksham":
+            return "Verification token missmatch", 403
+        return request.args['hub.challenge'], 200
+    return "Hello world", 200
 
 
 if __name__ == '__main__':
